866359034381793dd005e7a73b5f86aa1ca2d7e7
Psycho-cat <penedonarutofx2@gmail.com>
2016-09-02 22:59:59 +0100

    Pawn moves GG
    It all seems to work, pawn moves, has legal moves, move 2 at start, take
    pieces.

--- Chess Project.py
+++ Chess Project.py
@@ -2,84 +2,81 @@ class Board(object):
     # The chess board is represented as a 8x8 2D array
     def __init__(self):
         # Create the board and move pieces to their initial positions
-        self.__board = [[""] * 8 for i in range(8)]
+        self.__board = [[None] * 8 for i in range(8)]
         for i in range(0,8):
             self.__board[1][i] = Pawn("b")
             self.__board[6][i] = Pawn("w")
-            self.__board[0][0] = Rook("b")
-            self.__board[7][0] = Rook("w")
-            self.__board[0][1] = Knight("b")
-            self.__board[7][1] = Knight("w")
-            self.__board[0][2] = Bishop("b")
-            self.__board[7][2] = Bishop("w")
-            self.__board[0][3] = Queen("b")
-            self.__board[7][3] = Queen("w")
-            self.__board[0][4] = King("b")
-            self.__board[7][4] = King("w")
-            self.__board[0][5] = Bishop("b")
-            self.__board[7][5] = Bishop("w")
-            self.__board[0][6] = Knight("b")
-            self.__board[7][6] = Knight("w")
-            self.__board[0][7] = Rook("b")
-            self.__board[7][7] = Rook("w")
-        print(self.__board)
-        move_piece()
-        
+        self.__board[0][0] = Rook("b")
+        self.__board[7][0] = Rook("w")
+        self.__board[0][1] = Knight("b")
+        self.__board[7][1] = Knight("w")
+        self.__board[0][2] = Bishop("b")
+        self.__board[7][2] = Bishop("w")
+        self.__board[0][3] = Queen("b")
+        self.__board[7][3] = Queen("w")
+        self.__board[0][4] = King("b")
+        self.__board[7][4] = King("w")
+        self.__board[0][5] = Bishop("b")
+        self.__board[7][5] = Bishop("w")
+        self.__board[0][6] = Knight("b")
+        self.__board[7][6] = Knight("w")
+        self.__board[0][7] = Rook("b")
+        self.__board[7][7] = Rook("w")
 
-def move_piece():
-    accepted1 = ["a","b","c","d","e","f","g","h"]
-    accepted2 = ["1","2","3","4","5","6","7","8"]
+    def move(self, from_coords, to_coords):
+        piece = self.__board[from_coords[0]][from_coords[1]]
+        if piece is not None:  # Check if piece exists
+            if piece.check_legal_move(from_coords, self.__board, to_coords):  # Check if the move is legal
+                # Move the piece and then remove it from its original position
+                self.__board[to_coords[0]][to_coords[1]] = self.__board[from_coords[0]][from_coords[1]]
+                self.__board[from_coords[0]][from_coords[1]] = None
 
-    move_from = " "
-    move_to = " "
-    
-    while (move_from[0] not in accepted1) or (move_from[1] not in accepted2):
-        move_from = str(input("Please enter the piece you wish to move: "))
-        
-        if len(move_from) == 2: #length must be 2, otherwise code breaks
-            if move_from[0] in accepted1 or move_from[1] in accepted2: #check for format
-                print("") #Accepted
-            else:
-                print("Invalid")
-        else:
-            print("Invalid")
+    def board(self):
+        for row in self.__board:
+            print(row)
 
 
-    while move_to[0] not in accepted1 or move_to[1] not in accepted2:
-        move_to = str(input("please enter the desired destination of the piece: "))
-
-        if len(move_to) == 2: #length must be 2, otherwise code breaks
-            if move_to[0] in accepted1 or move_to[1] in accepted2: #check for format
-                print("") #Accepted
-            else:
-                print("Invalid")
-        else:
-            print("Invalid")
-            
-    print("Moving from",move_from,"to",move_to)
-
-    movefrom = str(accepted1.index(move_from[0])) + str(int(move_from[1])-1)
-    moveto = str(accepted1.index(move_to[0])) + str(int(move_to[1])-1)
-    print("Moving from",movefrom,"to",moveto)
-
-    row = board[movefrom[0]]
-    print(row[movefrom[1]])
-
-    
 class Piece(object):
     # A generalised piece object, chess pieces will inherit from this class
     # and override its procedures
+
     def __init__(self, piece, colour):
         self._piece = piece  # Pieces are named for AN/FEN
         self._colour = colour  # Pieces are given colours for each player
-        
+        self._legal_moves = []
+
+    def get_legal_moves(self, from_coords, board):
+        # A method to check for all of the possible moves for a given piece,
+        # given its current co-ordinates and the current position of the board.
+        pass
+
+    def check_legal_move(self, from_coords, board, to_coords):
+        self.get_legal_moves(from_coords, board)
+        print(self._legal_moves)
+        if to_coords in self._legal_moves:
+            return True
+        else:
+            return False
+
+    @property
+    def colour(self):
+        return self._colour
+
+    @property
+    def legal_moves(self):
+        return self._legal_moves
+
+    def __repr__ (self):
+        return str(self._colour + "General Piece")
+
+    
 class King(Piece):   # King piece class
     def __init__(self, colour):
         super().__init__("K", colour)
     def move(self):
         pass
     def __repr__(self):
-        return str(self._colour + "K")
+        return str(self._colour + "K  ")
         
 class Queen(Piece):    # Queen piece class
     def __init__(self, colour):
@@ -87,7 +84,7 @@ class Queen(Piece):    # Queen piece class
     def move(self):
         pass
     def __repr__(self):
-        return str(self._colour + "Q")
+        return str(self._colour + "Q  ")
         
 class Rook(Piece):  # Rook piece class
     def __init__(self, colour):
@@ -95,7 +92,7 @@ class Rook(Piece):  # Rook piece class
     def move(self):
         pass
     def __repr__(self):
-        return str(self._colour + "R")
+        return str(self._colour + "R  ")
     
 class Bishop(Piece):    # Bishop piece class
     def __init__(self, colour):
@@ -103,7 +100,7 @@ class Bishop(Piece):    # Bishop piece class
     def move(self):
         pass
     def __repr__(self):
-        return str(self._colour + "B")
+        return str(self._colour + "B  ")
         
 class Knight(Piece):    # Knight piece class
     def __init__(self, colour):
@@ -111,16 +108,81 @@ class Knight(Piece):    # Knight piece class
     def move(self):
         pass
     def __repr__(self):
-        return str(self._colour + "N")
+        return str(self._colour + "N  ")
         
 class Pawn(Piece):
-    # Pawn piece class 
+    # Pawn piece class
+    
     def __init__(self, colour):
             super().__init__("p", colour)
-    def move(self):
-        pass
-    def __repr__(self):
-        return str(self._colour + "p")
-    
-board = Board()
+            
+    def get_legal_moves(self, from_coords, board):
 
+        self._legal_moves = [] # Empties the list from previous moves
+        
+        if self._colour == "w":
+            
+            if board[from_coords[0]-1][from_coords[1]] is None:
+                self._legal_moves.append([from_coords[0]-1, from_coords[1]])
+                if from_coords[0] == 6 and board[4][from_coords[1]] is None:
+                    self._legal_moves.append([4, from_coords[1]])
+                    
+            # Check such that all but the left-most pawn can legally make his move
+            if from_coords[1] != 0:
+                # Check if the diagonal-right square is occupied
+                if board[from_coords[0]-1][from_coords[1]-1] is not None:
+                    # Check if the colour of the piece is white
+                    if board[from_coords[0]-1][from_coords[1]-1].colour == "w":
+                        # If so, add the capture into legal moves
+                        self._legal_moves.append([[from_coords[0]-1], from_coords[1]-1])
+
+            # Check such that all but the right-most pawn can legally make this move
+            if from_coords[1] != 7:
+                # Check if the diagonal-right square is occupied        
+                if board[from_coords[0]-1][from_coords[1]-1] is not None:
+                    # Check if the colour of the piece is white
+                    if board[from_coords[0]-1][from_coords[1]-1].colour == "w":
+                        # If so, add the capture into legal moves
+                        self._legal_moves.append([from_coords[0]-1, from_coords[1]-1])
+
+        
+        if self._colour == "b":
+
+            # Check if the square directly below is empty
+            if board[from_coords[0]+1][from_coords[1]] is None:
+                # If so, add the move to the pawn's legal moves
+                self._legal_moves.append([from_coords[0]+1, from_coords[1]])
+
+                # Then check if the pawn is on its starting rank 
+                # and the square two spaces below is empty
+                if from_coords[0] == 1 and board[3][from_coords[1]] is None:
+                    # If so, add the option of moving two squares to legal moves
+                    self._legal_moves.append([3, from_coords[1]])
+                    
+            # Check such that all but the left-most pawn can legally make his move
+            if from_coords[1] != 0:
+                # Check if the diagonal-right square is occupied
+                if board[from_coords[0]+1][from_coords[1]-1] is not None:
+                    # Check if the colour of the piece is white
+                    if board[from_coords[0]+1][from_coords[1]-1].colour == "w":
+                        # If so, add the capture into legal moves
+                        self._legal_moves.append([from_coords[0]+1, from_coords[1]-1])
+
+            # Check such that all but the right-most pawn can legally make this move
+            if from_coords[1] != 7:
+                # Check if the diagonal-right square is occupied        
+                if board[from_coords[0]+1][from_coords[1]+1] is not None:
+                    # Check if the colour of the piece is white
+                    if board[from_coords[0]+1][from_coords[1]+1].colour == "w":
+                        # If so, add the capture into legal moves
+                        self._legal_moves.append([from_coords[0]+1, from_coords[1]+1])
+
+    
+    def __repr__(self):
+        return str(self._colour + "p  ")
+
+def a():
+    global board
+    board = Board()
+def b():
+    board.board()
